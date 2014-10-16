85bc806895c9e6a8a24e2f568a5b7ab8d71f3579
Hyun Seo <hjseo767@gmail.com>
2014-10-15 21:26:42 -0500

    Voting.py

---
+++ RunVoting.py
@@ -0,0 +1,5 @@
+import sys
+
+from Voting import voting_solve
+
+voting_solve(sys.stdin, sys.stdout)
--- Voting.py
+++ Voting.py
@@ -1,3 +1,5 @@
+## ========== classes =============
+
 class Candidate:
     list_of_current_candis = []
     list_of_elim_candis = []
@@ -6,23 +8,8 @@ class Candidate:
         self.ballot_list = []
 
     def is_elim(self):
-        return (self.candi_name is in list_of_elim_candis)
+        return (self.candi_name in list_of_elim_candis)
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 class Ballot():
     def __init__(self,ballot_list):
@@ -34,24 +21,30 @@ class Ballot():
             yield ballot_list[i]
             i += 1 
 
+# ==================== CODE =========================
 
 
 
+def voting_solve (r, w) :
+    # number of cases following
+    cases = r.readline()   
+    blank = r.readline()
 
+    l = r.readline()
+#    l = l.split()
+    print(l)
+    num_candis = int(l)
 
-def voting_solve(r,w):
-    r.readline()
-    r.readline()
-    line = r.readline()
-    l = line.split()
-    num_candis = int(l[0])
-    list_of_lines = r.readlines()
-    for line in list_of_lines[]:
-        name = line.split()
+    for i in range(num_candis):
+        name = r.readlines()
+
+    #    name = line.split("\n")
         candi = Candidate(name)
+        print(candi)
+
+    list_of_lines = r.readlines()
+
+    for line in list_of_lines:
+        ballot = Ballot(line)
 
 
-
-
-
-    for line in 
