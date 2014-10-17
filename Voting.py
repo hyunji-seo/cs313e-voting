e8f97c16a8f940c5884b4b571b0c748b1f388a07
Jiannan Zhang <jz1992@cs.utexas.edu>
2014-10-16 19:41:26 -0500

    half way done

--- Voting.py
+++ Voting.py
@@ -1,11 +1,10 @@
 ## ========== classes =============
 
 class Candidate:
-    list_of_current_candis = []
-    list_of_elim_candis = []
     def __init__(self,candi_name):
         self.name = candi_name
         self.ballot_list = []
+        self.count = 0
 
     def is_elim(self):
         return (self.candi_name in list_of_elim_candis)
@@ -13,7 +12,7 @@ class Candidate:
 
 class Ballot():
     def __init__(self,ballot_list):
-        self.ballot = ballot_list
+        self.ballot_list = ballot_list
 
     def next_choice(self):
         i = 0
@@ -26,25 +25,57 @@ class Ballot():
 
 
 def voting_solve (r, w) :
+    num_ballots = 0
+    list_of_current_candis = []
+    list_of_elim_candis = []
     # number of cases following
     cases = r.readline()   
     blank = r.readline()
 
     l = r.readline()
-#    l = l.split()
     print(l)
     num_candis = int(l)
 
+    dic_name_to_object = {}
     for i in range(num_candis):
         name = r.readlines()
-
-    #    name = line.split("\n")
         candi = Candidate(name)
-        print(candi)
+        dic_name_to_object[name] = candi
+        list_of_current_candis.append(candi)
+        #print(candi)
+    list_of_items = zip(range(1,num_candis + 1),list_of_current_candis)
+    dic_ballot_to_candi = {k : v for k, v in list_of_items}
 
-    list_of_lines = r.readlines()
+    # the remaining 
+    list_of_ballots = r.readlines()
 
-    for line in list_of_lines:
+    for line in list_of_ballots:
         ballot = Ballot(line)
+        ballot_iter = next_choice(ballot)
+        first_choice = next(ballot_iter)
+        candi_name = dic_ballot_to_candi[first_choice]
+        candi_object = dic_name_to_object[candi_name]
+        candi_object.ballot_list.append(ballot.ballot_list)
+        candi_object.count += 1
+        num_ballots += 1
 
+
+    # first 2 terminating conditions
+    cutoff = num_ballots // 2 + 1
+    cutoff_tie = num_ballots / num_candis
+    if type(cutoff_tie) is int:
+        all_tied = True
+        for candi in list_of_current_candis:
+            if (candi.count != cutoff_tie):
+                all_tied = False
+
+    if (all_tied == True):
+        print list_of_current_candis
+
+    for candi in list_of_current_candis:
+        if (candi.count >= cutoff):
+            return candi.name
+
+    # now go to cool part!
+    
 
