0ac334291eceff507577c6aaa367a79a74d63d6b
Jiannan Zhang <jz1992@cs.utexas.edu>
2014-10-22 18:09:03 -0500

    add test case

---
+++ TestVoting.py
@@ -0,0 +1,78 @@
+from io import StringIO
+from unittest import main,TestCase
+from Voting import voting_solve,has_winner,Candidate,Ballot
+
+class TestVoting(TestCase):
+
+    def test_Candidate_class1(self):
+        p = Candidate("a")
+        self.assertEqual(p.name, "a")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # below are test for ballot class
+    def test_Ballot_class1(self):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   # below are tests for has_winner
+    def test_has_winner1(self):  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # below are test cases for voting solve
+    def test_voting_solve1(self):
+
+
+
+
+
